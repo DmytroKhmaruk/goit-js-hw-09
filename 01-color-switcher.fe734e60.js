6d68c98b40a552c619f0cc2794989573a1ee960d
DmytroKhmaruk <120748680+DmytroKhmaruk@users.noreply.github.com>
2023-06-14 15:50:58 +0000

    Deploying to gh-pages from @ DmytroKhmaruk/goit-js-hw-09@a8663ddbc0b9b4566364c8d83ed12ca92136c32b 🚀

--- 01-color-switcher.fe734e60.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]"),e=document.body;t.addEventListener("click",(function(){!0===t.disabled&&(a.disabled=!1),setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"));e.style.backgroundColor=t}),1e3)}))}();
-//# sourceMappingURL=01-color-switcher.fe734e60.js.map
